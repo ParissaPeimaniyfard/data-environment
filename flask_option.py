9bf7744692578d64ce29fb6ef0c14b40d2cda8f0
Parissa Peimaniyfard <zahra.pmf@gmail.com>
2023-10-22 19:49:32 +0200

    the start function defined

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,12 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    # FFLASK_ENV=development python flask_option.py
+
+    environment_variable = os.getenv("FLASK_ENV", "empty")
+    return f"Starting in {environment_variable} mode..."
+
+
 
 if __name__ == "__main__":
     print(start())
